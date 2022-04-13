4a44c9b2c28c981e62dcf0b03e1ff7957566430c
otdot <s2200126@BC-C02GR0BSQ6LR-MacBook-Air.local>
2022-04-13 16:22:47 +0300

    mvp

--- index.php
+++ index.php
@@ -1,18 +1,47 @@
 <?php 
-include ("./db.php");
+    include ("./db.php");
 
-if (isset($_POST["submit"])) {
-    $todo = $_POST["todo"];
-
-    $query = "INSERT INTO tasks (task) VALUES ('$todo')";
-    $result = mysqli_query($connection, $query);
-
-    if (!$result) {
+    $alldataquery = "SELECT * FROM tasks";
+    $alldata = mysqli_query($connection, $alldataquery);
+    if (!$alldata) {
         die("query failed");
     }
-}
+    
 
+    if (isset($_POST["submit2"])) {
+        $todo = $_POST["todo"];
+        $query = "INSERT INTO tasks (task) VALUES ('$todo')";
+        $result = mysqli_query($connection, $query);
 
+        if (!$result) {
+            die("query failed");
+        }
+
+        $alldataquery = "SELECT * FROM tasks";
+        $alldata = mysqli_query($connection, $alldataquery);
+        if (!$alldata) {
+            die("query failed");
+        }
+
+    }
+
+    if (isset($_POST["deletebutton"])) {
+        $id = $_POST['delete'];
+
+        $query = "DELETE FROM tasks WHERE id = $id";
+        $result = mysqli_query($connection, $query);
+
+        if (!$result) {
+            die("query failed");
+        }
+
+        $alldataquery = "SELECT * FROM tasks";
+        $alldata = mysqli_query($connection, $alldataquery);
+        if (!$alldata) {
+            die("query failed");
+        }
+
+    }
 ?>
 
 <!DOCTYPE html>
@@ -22,13 +51,25 @@ if (isset($_POST["submit"])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>hello</h1>
+    <h1>Add and delete tasks</h1>
     <form action="index.php" method="POST">
         <label for="todo">todo</label>
         <input id="todo" name="todo" type="text">
-        <input name="submit" type="submit" value="add todo">
+        <input name="submit2" type="submit" value="add todo">
     </form>
+    <?php
+    while ($row2 = mysqli_fetch_assoc($alldata)) { ?>
+        <div class="todoitem">
+            <p type="text" data-id=<?php echo $row2['id']?>><?= $row2['task'] ?></p>
+            <form action="index.php" method="POST">
+                <input hidden value="<?php echo $row2['id']?>" name="delete"></input>
+                <input class="deletebutton" value="delete" name="deletebutton" type="submit">
+            </form>
+        </div>
+        <?php
+		} ?>
 </body>
 </html>
